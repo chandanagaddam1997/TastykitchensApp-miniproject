3327dbbc92658a8420c3b6cc67cef9804e571b35
chandanagaddam1997 <chandanagaddam77@gmail.com>
2023-06-20 03:34:48 +0000

    solution

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import Header from '../Header'
+
+import AllRestaurantSection from '../AllRestaurantSection'
+import Footer from '../Footer'
+
+const Home = () => (
+  <>
+    <Header />
+    <AllRestaurantSection />
+    <Footer />
+  </>
+)
+export default Home
